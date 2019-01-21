5f0558b4fe27f27d8c3a0f0e4a1f543f417c4821
alethecine96 <34508076+alethecine96@users.noreply.github.com>
2019-01-21 23:20:11 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/menu.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alessandro/CLionProjects/menu1/menu/Drawable.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Drawable.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Enemy.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Enemy.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Eyes.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Eyes.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Inventory.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Inventory.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/MapLoader.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/MapLoader.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Mappa.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Mappa.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Observer.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Observer.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Player.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Player.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/Projectile.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/Projectile.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/main.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/main.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/map.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/map.cpp.o"
+  "/home/alessandro/CLionProjects/menu1/menu/menu.cpp" "/home/alessandro/CLionProjects/menu1/menu/cmake-build-debug/CMakeFiles/menu.dir/menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/menu.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menu.dir/main.cpp.o"
+  "CMakeFiles/menu.dir/menu.cpp.o"
+  "CMakeFiles/menu.dir/Inventory.cpp.o"
+  "CMakeFiles/menu.dir/map.cpp.o"
+  "CMakeFiles/menu.dir/Mappa.cpp.o"
+  "CMakeFiles/menu.dir/Player.cpp.o"
+  "CMakeFiles/menu.dir/Eyes.cpp.o"
+  "CMakeFiles/menu.dir/MapLoader.cpp.o"
+  "CMakeFiles/menu.dir/Projectile.cpp.o"
+  "CMakeFiles/menu.dir/Enemy.cpp.o"
+  "CMakeFiles/menu.dir/Drawable.cpp.o"
+  "CMakeFiles/menu.dir/Observer.cpp.o"
+  "menu.pdb"
+  "menu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/menu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
